75fe3815cc29df4f804152ee2cda7157e29e011c
Zach-Attach <zachlaborde93@gmail.com>
2023-04-10 22:35:26 -0400

    More cleanups

--- run.py
+++ run.py
@@ -2,28 +2,41 @@ from CTRNN import CTRNN
 from stochsearch import EvolSearch
 import numpy as np
 import matplotlib.pyplot as plt
+import warnings
+
+warnings.simplefilter('ignore', RuntimeWarning) # ignore divide by zero warnings
 
 # evolve a net that is capable of switching to two modes from an external manipulation
 
 # CONSTANTS
 WAVELENGTH: float = 4*np.pi # 2pi is the wavelength of a sin/cos wave
-INIT_STATE: float = 0.5 # initial state of all neurons
 STEP_SIZE: float = 0.01 # step size for the euler integration
 NUM_GENS: int = 100 # number of generations to run the evolutionary search
 NUM_TIME_UNITS: float = 40. # number of time units to run the CTRNN for
 NUM_STEPS: int = int(NUM_TIME_UNITS/STEP_SIZE) # number of steps to run the CTRNN for
 POP_SIZE: int = 100 # population size
 NUM_NODES = 4 # number of neurons in the CTRNN
-GENE_SCALES: list = [1,32,32] # scales each gene for the taus, biases, and weights
+GENE_SCALES: list[int] = [1,32,32] # scales each gene for the taus, biases, and weights
 CHOSEN_FUNC: str = 'derivative' # the fitness function to use
-# GENE_SCALES: list = [1,16,16] # scales each gene for the taus, biases, and weights
 def wave(x): return np.cos(x * 3) # the wave to be predicted
 MODES: list = [np.sin, np.cos] #wave] # the two modes of behavior
+
+X_AXIS: np.ndarray[float] = np.linspace(0., WAVELENGTH, NUM_STEPS)
+INIT_STATE: float = 0.5 # initial state of all neurons
 
 
 FITNESS_FUNCS: dict = {
   'diff': lambda states, yhat: np.abs(states-yhat).sum(), # difference between the predicted and expected output
-  'derivative': lambda states, yhat: np.abs(np.diff(states[-500:])-np.diff(yhat[-500:])).sum(), # difference between the derivative of the predicted and expected output
+  'derivative': lambda states, yhat: np.abs(np.diff(states[int(-.9*NUM_STEPS):])-np.diff(yhat[int(-.9*NUM_STEPS):])).sum(), # difference between the derivative of the predicted and expected output
+}
+
+# defining the parameters for the evolutionary search
+EVOL_PARAMS: dict = {
+    'num_processes' : 8, # (optional) number of proccesses for multiprocessing.Pool
+    'pop_size' : POP_SIZE,    # population size
+    'genotype_size': NUM_NODES*2+NUM_NODES**2, # dimensionality of solution
+    'elitist_fraction': 0.04, # fraction of population retained as is between generations
+    'mutation_variance': 0.2 # mutation noise added to offspring.
 }
 
 # Helper Functions
@@ -31,9 +44,9 @@ FITNESS_FUNCS: dict = {
 def adjustWave(x: np.ndarray[np.float64]) -> np.ndarray[np.float64]: # adjust the wave to be between 0 and 1
   return (x+1.)/2.
 
-def getOutputs(cns, input, steps=2000) -> np.ndarray[np.float64]: # get the outputs of the last neuron in the CTRNN after inputing an input value to the 1st neuron
-  states = np.array([])
-  inputArr = np.zeros(NUM_NODES)
+def getOutputs(cns: CTRNN, input: int) -> np.ndarray[np.float64]: # get the outputs of the last neuron in the CTRNN after inputing an input value to the 1st neuron
+  states: np.ndarray = np.array([])
+  inputArr: np.ndarray[np.int32] = np.zeros(NUM_NODES)
   inputArr[0] = input
 
   for _ in range(NUM_STEPS):
@@ -41,57 +54,51 @@ def getOutputs(cns, input, steps=2000) -> np.ndarray[np.float64]: # get the outp
     states: np.ndarray[np.float64] = np.append(states, cns.outputs[-1])
   return states
 
-def fitness(cns, f:str = None):
+def fitness(cns: CTRNN, f: str) -> list:
   fitness: float = 0.
-  samplePoints: np.ndarray[float] = np.linspace(0., WAVELENGTH, NUM_STEPS)
-  states: list = []
-  yhats = []
+
+  states: list[np.ndarray[np.float64]] = []
+  yhats: list[np.ndarray] = []
 
   for i, measure in enumerate(MODES):
-    yhats.append(adjustWave(measure(samplePoints))) # get the expected output
+    yhats.append(adjustWave(measure(X_AXIS))) # get the expected output
 
     cns.states = np.full(NUM_NODES, INIT_STATE) # reset the states of all neurons
-    states.append(getOutputs(cns, i, NUM_STEPS)) # get the outputs of the CTRNN
+    states.append(getOutputs(cns, i)) # get the outputs of the CTRNN
 
-    if f:
-      fitness += FITNESS_FUNCS[f](states[-1], yhats[-1])
+    fitness += FITNESS_FUNCS[f](states[-1], yhats[-1])
   
-  return fitness, states, yhats, samplePoints
-    
-def plot(cns):
-  _, ypred, yhat, x = fitness(cns)
+  return fitness, states, yhats
 
-  for i in range(len(MODES)):
-    plt.plot(x, yhat[i], 'r', label='Expected')
-    plt.plot(x, ypred[i], 'b', label='CTRNN')
-    plt.show()
-
+def geno2pheno(genome) -> list[np.ndarray]:
+  genes: list[np.ndarray] = np.split(genome, [NUM_NODES,2*NUM_NODES]) # split the genome
+  taus, biases, weights = [GENE_SCALES[i]*genes[i] for i in range(3)] # scale genes values to appropriate ranges
+  biases -= GENE_SCALES[1]/2 # shift the biases to be between -16 and 16
+  weights -= GENE_SCALES[2]/2 # shift the weights to be between -16 and 16
+  weights = np.reshape(weights, (NUM_NODES,NUM_NODES)) # reshape the weights to be a square matrix
+  return taus, biases, weights
 
 def makeCTRNN(genome):
   cns = CTRNN(NUM_NODES,step_size=STEP_SIZE) # create a CTRNN of {size} neurons
 
-  genes = np.split(genome, [NUM_NODES,2*NUM_NODES]) # split the genome
-  cns.taus, biases, weights = [GENE_SCALES[i]*genes[i] for i in range(3)] # scale genes values to appropriate ranges
-  cns.biases = biases-16 # shift the biases to be between -16 and 16
-  weights = weights-16 # shift the weights to be between -16 and 16
-  cns.weights = np.reshape(weights, (NUM_NODES,NUM_NODES)) # reshape the weights to be a square matrix
+  cns.taus, cns.biases, cns.weights = geno2pheno(genome) # set the taus, biases, and weights of the CTRNN``
 
   return cns
 
-def eval(genome):
-  cns = makeCTRNN(genome)
+def eval(genome: np.ndarray):
+  cns: CTRNN = makeCTRNN(genome)
   f = fitness(cns, CHOSEN_FUNC)[0]
   return 0 if np.isnan(f) else 1./f
 
-# defining the parameters for the evolutionary search
-EVOL_PARAMS: dict = {
-    'num_processes' : 8, # (optional) number of proccesses for multiprocessing.Pool
-    'pop_size' : POP_SIZE,    # population size
-    'genotype_size': NUM_NODES*2+NUM_NODES**2, # dimensionality of solution
-    'fitness_function': eval, # custom function defined to evaluate fitness of a solution
-    'elitist_fraction': 0.04, # fraction of population retained as is between generations
-    'mutation_variance': 0.2 # mutation noise added to offspring.
-}
+EVOL_PARAMS['fitness_function'] = eval # custom function defined to evaluate fitness of a solution
+
+def plot(cns: CTRNN):
+  _, ypred, yhat = fitness(cns, CHOSEN_FUNC)
+
+  for i in range(len(MODES)):
+    plt.plot(X_AXIS, yhat[i], 'r', label='Expected')
+    plt.plot(X_AXIS, ypred[i], 'b', label='CTRNN')
+    plt.show()
 
 es = EvolSearch(EVOL_PARAMS)
 
